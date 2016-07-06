89ecf8f0d37a43c852c586b5d34de073dd73f876
Exterras <choi3377@naver.com>
2016-07-07 00:47:48 +0900

    20160707 00:47:00 AlphaFriends ver 1.0.0

---
+++ app/src/main/java/com/github/exterras/alphafriends/MainActivity.java
@@ -0,0 +1,21 @@
+package com.github.exterras.alphafriends;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView1 = (TextView) findViewById(R.id.textView1);
+    }
+
+    public void onButton1Clicked(View v){
+        textView1.setText("Welcome!");
+    }
+}
